4c80851da7522412b171fdd57acc0e6483e3065e
urieco <urieco@github.com>
2022-09-14 00:11:23 +0700

    Add the two color modes

--- README.md
+++ README.md
@@ -2,25 +2,23 @@ Goal: A browser version of something between a sketchpad and an Etch-A-Sketch (T
 
 General Outline:
 
-**Finished**: 
+**Finished**: (The required features have been added)
 - Make a drawing area with 16x16 grid of square divs (Using JS and Flexbox instead of manually placing them). 
     + Use for... loop to generate 256 squares. 
 
 - Set up a "hover" effect ("mouseenter") so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail trhough your grid like a pen would. 
     + Later on, set it up with a "click" effect instead, to accurately draw out what you have in mind. The "hover" will be there to help the user determine where they are currently in the drawing board - a tracking tool.  
 
-**To-do**:
 - Add a button at the top of the page that will send the user a popup asking for the number of squares per side for the new grid. 
     + Popup using prompt() should be replaced with input field. 
     + Set the limit the user input to a maximum of 100 (and no negative value or non-integer)
     + Add a button to remove the existing grid.
         + Once entered, the existing grid should be removed and the new grid should be generated in the same total space as before, so that you've got a new sketch pad. 
-        + Remove the button and set up the function to automatically remove the grid before applying a new grid. 
-    + Add a mode to keep your previous drawing and push them down or sideways. 
-    
+        + Remove the button and set up the function to automatically remove the grid before applying a new grid.    
 
 - Extra Credit: Instead of just changing the color of a square from white to black (for example), have each pass through with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
 
+**To-do**: 
 
 - Make a color palette consisting of 12 colors: Red, Red-Orange, Orange, Yellow-Orange, Yellow, Yellow-Green, Green, Blue-Green, Blue, Blue-Violet, Violet/Purple and Red-Violet. 
     + Small squares (or circles) sit close to each other.
@@ -29,3 +27,5 @@ General Outline:
     + In the event of a 'click', change the color value of the function that colors that drawing squares. 
 
 - Custom cursor in the page (or specifically the drawing area).
+- Add a mode to keep your previous drawing and push them down or sideways. 
+- Decorate the site.
--- script.js
+++ script.js
@@ -2,54 +2,105 @@ const sketchpad = document.querySelector(".sketchpad");
 const drawingBoard = document.querySelector(".drawing-board");
 
 //default size: 16x16
-let area = 256; 
-// generateBoard(area);
+let area = 256;
 
 const userInput = document.querySelector(".user-input");
 const button = document.querySelector("#select-button");
 button.addEventListener("click", getBoardSide);
 
-const removeButton = document.querySelector("#remove-button");
-removeButton.addEventListener("click", (e) => {
-    const squares = document.querySelectorAll(".drawing-board > .drawing-square");
-    squares.forEach(square => square.remove());
+const randomizeColorButton = document.querySelector("#randomize-color");
+const shadesOfBlackButton = document.querySelector("#shades-of-black");
+
+let randomize = false;
+let shade = false;
+
+randomizeColorButton.addEventListener("click", (e) => {
+    randomize = true;
+    shade = false;
+    randomizeColorButton.classList.add("selected");
+    shadesOfBlackButton.classList.remove("selected");
+});
+shadesOfBlackButton.addEventListener("click", (e) => {
+    randomize = false;
+    shade = true;
+    shadesOfBlackButton.classList.add("selected");
+    randomizeColorButton.classList.remove("selected");
 });
 
 const warning = document.createElement('div');
 warning.setAttribute("id", "warning");
 warning.classList.add("hidden");
-userInput.appendChild(warning);  
+userInput.appendChild(warning);
 warning.innerText = "(The value you have input is not supported)";
 
 function getBoardSide() {
+    removeBoard();
     //input tag always returns a string
     let boardSide = document.getElementById("board-side").value;
-    
+
     //prevent multiple clicking with invalid value
     warning.classList.add("hidden");
-    if (typeof +boardSide !== "number"|| !Number.isInteger(+boardSide) || boardSide < 0 || boardSide > 100) {
+    if (typeof +boardSide !== "number" || !Number.isInteger(+boardSide) || boardSide < 0 || boardSide > 100) {
         warning.classList.remove("hidden");
         return;
     }
-    area = boardSide*boardSide;
+    area = boardSide * boardSide;
     generateBoard(area);
 }
 
 function generateBoard(area) {
-    let newSide = Math.sqrt((960*960)/area); 
+    let newSide = Math.sqrt((960 * 960) / area);
     for (let i = area; i > 0; i--) {
-        const drawingSquare = document.createElement("div"); 
-        drawingBoard.appendChild(drawingSquare);   
+        const drawingSquare = document.createElement("div");
+        drawingBoard.appendChild(drawingSquare);
         drawingSquare.classList.add("drawing-square");
-        drawingSquare.style.cssText = `width: ${newSide}px; height: ${newSide}px;`;
+
+        const root = document.querySelector(":root");
+        const rootStyles = getComputedStyle(root);
+        root.style.setProperty("--squarewidth", `${newSide}px`);
+        root.style.setProperty("--squareheight", `${newSide}px`);
     }
 
-    //track the mouse. We have to include this specifically in this function, otherwise including this on global scope can only only track the default drawing board of 16x16     
+    // Track the mouse. We have to include this declaration specifically in this function. Declaring this on global scope causes functions to stop working. Therefore, a redeclaration has to be made for function removeBoard().
     const squares = document.querySelectorAll(".drawing-square");
-    squares.forEach(square => square.addEventListener("mouseenter", (e) => {
-    square.classList.add("color-black");
-    }));
+
+    if (!randomize && !shade) {
+        squares.forEach(square => square.addEventListener("mouseenter", (e) => {
+            square.classList.add("color-black");
+        }));
+    } else if (randomize) {
+        squares.forEach(square => square.addEventListener("mouseenter", randomizeColor));
+
+        //let the squares keep the colors assigned when the mouse first passed through
+        squares.forEach(square => square.addEventListener("mouseleave", (e) => square.removeEventListener("mouseenter", randomizeColor)));
+    } else if (shade) {
+        squares.forEach(square => square.addEventListener("mouseenter", shadesOfBlack));
+        squares.forEach(square => square.addEventListener("mouseleave", (e) => square.removeEventListener("mouseenter", shadesOfBlack)));
+    }
 }
 
+function removeBoard() {
+    const squares = document.querySelectorAll(".drawing-board > .drawing-square");
+    squares.forEach(square => square.remove());
+}
 
+function randomizeColor() {
+    let rgbRed = Math.floor(Math.random() * 256);
+    let rgbGreen = Math.floor(Math.random() * 256);
+    let rgbBlue = Math.floor(Math.random() * 256);
+    this.style.cssText = `background-color: rgb(${rgbRed}, ${rgbGreen}, ${rgbBlue})`;
+}
+
+let alpha = 0;
+let alphaAdd = 1;
+let alphaMax = 1;
+
+function shadesOfBlack() {
+    if (alpha >= 0) {
+        alpha = (alpha * 10 + alphaAdd) / 10;
+        if (alpha >= alphaMax) alphaAdd = -1;
+        if (alpha <= 0) alphaAdd = 1;
+    }
+    this.style.cssText = `background-color: rgb(0, 0, 0); opacity: ${alpha};`;
+}
 
